f26cbe19f9d54d9f86e063dc0ef91a692f6143f0
sharminsiddique <132340729+sharminsiddique@users.noreply.github.com>
2023-05-04 19:07:36 +0100

    Assignment

--- dicerollingpractice.py
+++ dicerollingpractice.py
@@ -8,7 +8,7 @@ while message1 == answer2:
 if message1 == answer1:
     import random
     print(random.randrange(1, 7))
-    message2 = input("Would you like to roll again? yes or no?")
-    print(message2)
-    answer3 = input("yes")
-    answer4 = input("no")
+message2 = input("Would you like to roll again? yes or no?")
+print(message2)
+answer3 = input("yes")
+answer4 = input("no")
