ba17e3a459328ff908819a663c55541129c4212e
sharminsiddique <132340729+sharminsiddique@users.noreply.github.com>
2023-05-11 17:34:11 +0100

    Final

--- madlibsgen.py
+++ madlibsgen.py
@@ -1,6 +1,6 @@
 # Take information from user and fit it into story
 
-name_game = input("What is your name? ")
+name = input("What is your name? ")
 city = input("Which city do you want to be? ")
 animal = input("Which animal do you like the most? ")
 place = input ("Where do you want to be? ")
@@ -9,7 +9,7 @@ mood = input ("What is your mood? ")
 with_who = input ("Who do you want to be with? ")
 punish = input ("What do you want to do to punish them? ")
 
-story = f"\nI'am {name_game.title()}. "
+story = f"\nI'am {name.title()}. "
 story += f"I'am living in {city.title()} with {with_who.title()}. "
 story += f"I feel {mood}. \nI lost my {animal} when I was {activity}."
 story += f"\nI called {with_who} and they said they had eaten {animal}. "
